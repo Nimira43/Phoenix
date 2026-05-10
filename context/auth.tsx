df3fdf5a0624ef3a472be90fd0715ae92c0f1a79
Nimira43 <n.rabone72@aol.com>
2026-05-10 05:06:29 +0100

    Add admin role to use and save auth tokens in cookies. Conditionally render menu items depending if user is admin or not.

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { auth } from '@/firebase/client'
-import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth'
+import { GoogleAuthProvider, ParsedToken, signInWithPopup, User } from 'firebase/auth'
 import { createContext, useContext, useEffect, useState } from 'react'
+import { removeToken, setToken } from './actions'
 
 type AuthContextType = {
   currentUser: User | null
   logout: () => Promise<void>
   loginWithGoogle: () => Promise<void>
+  customClaims: ParsedToken | null
 }
 
 const AuthContext = createContext<AuthContextType | null>(null)
@@ -18,10 +20,27 @@ export const AuthProvider = ({
   children: React.ReactNode
 }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null)  
-  
+  const [customClaims, setCustomClaims] = useState<ParsedToken | null>(null)
+
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
       setCurrentUser(user ?? null)
+      if (user) {
+        const tokenResult = await user.getIdTokenResult()
+        const token = tokenResult.token
+        const refreshToken = user.refreshToken
+        const claims = tokenResult.claims
+        setCustomClaims(claims ?? null)
+        
+        if (token && refreshToken) {
+          await setToken({
+            token,
+            refreshToken
+          })
+        }
+      } else {
+        await removeToken()
+      }
     })
     return () => unsubscribe()
   }, [])
@@ -40,7 +59,8 @@ export const AuthProvider = ({
       value={{
         currentUser,
         logout,
-        loginWithGoogle
+        loginWithGoogle,
+        customClaims
       }}
     >
       {children}
